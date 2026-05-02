9ee0b3abea3aa8e4722275787a26f93d75ce0b16
1bitcode <38600431+hanbit10@users.noreply.github.com>
2026-05-02 09:42:54 +0000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div >
-      Hello
+      <h1 className="poppins font-bold">SoloExit website</h1>  
     </div>
   );
 }
